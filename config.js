d1179cbd4263da621fe8159d8160b7011889d2d0
mbsprojectcalculator-source <mbsprojectcalculator@gmail.com>
2026-06-30 10:15:18 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.ALARM_REMINDER_CONFIG = {
+  // Paste the deployed Google Apps Script Web App /exec URL here.
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbysXOeoOGuuk6ge628aZnoQxw-NKLIHgGHBDZdLxHk40mfTW84dky4rpjaG_H-dqkkM/exec",
+
+  timezone: "Asia/Singapore",
+  timeZoneOffset: "+08:00",
+  refreshMs: 60000,
+  alarmWindowMinutes: 60
+};
